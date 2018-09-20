339ff97ac59603c4186001ad22d35abd3b833d27
Dhananjay Singh <dhananjay1204@gmail.com>
2018-09-21 00:00:02 +0530

    simple producer

---
+++ simple-spring/simple-producer/src/main/java/com/mynotes/spring/kafka/simpleproducer/SimpleProducerApplication.java
@@ -0,0 +1,12 @@
+package com.mynotes.spring.kafka.simpleproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleProducerApplication.class, args);
+	}
+}
